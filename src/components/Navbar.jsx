757955197606c06edfd8aa3c516649e2d5fcd192
abhrajitray77 <abhrajitray77@gmail.com>
2023-08-24 23:27:04 -0700

    removed tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,14 @@
 import React from "react";
+import './../index.css'
 
 const Navbar = () => {
   return (
-    <nav className="flex md:flex-row flex-col md:justify-between
-    md:space-y-0 space-y-2 md:px-4 items-center bg-[#fc86c7]
-    py-4">
-      <h1 className="text-slate-100
-      font-[GreatVibesReg] text-2xl
-      ">Elena Joy</h1>
-        <ul className="flex md:space-x-6 space-x-24
-        text-slate-100 font-semibold">
-          <li className="cursor-pointer">About</li>
-          <li className="cursor-pointer">Work</li>
-          <li className="cursor-pointer">Contact</li>
+    <nav className="navbar">
+      <h1 class="logo">Elena Joy</h1>
+        <ul class="nav-ul">
+          <li class="nav-li">About</li>
+          <li class="nav-li">Work</li>
+          <li class="nav-li">Contact</li>
         </ul>
     </nav>
   );
