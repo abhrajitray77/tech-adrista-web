a420d5b5c7c4c34e15f1ae9901a78fb4a71e1aff
abhrajitray77 <abhrajitray77@gmail.com>
2023-08-24 23:17:23 -0700

    added fonts and nav changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <>
-    <header>
-      <Navbar />
-    </header>
- 
-    </>
-  )
+    <div>
+      <header>
+        <Navbar />
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav>
-      <h1>Elena Joy</h1>
-        <ul>
-          <li>About</li>
-          <li>Work</li>
-          <li>Contact</li>
+    <nav className="flex md:flex-row flex-col md:justify-between
+    md:space-y-0 space-y-2 md:px-4 items-center bg-[#fc86c7]
+    py-4">
+      <h1 className="text-slate-100
+      font-[GreatVibesReg] text-2xl
+      ">Elena Joy</h1>
+        <ul className="flex md:space-x-6 space-x-24
+        text-slate-100 font-semibold">
+          <li className="cursor-pointer">About</li>
+          <li className="cursor-pointer">Work</li>
+          <li className="cursor-pointer">Contact</li>
         </ul>
     </nav>
   );
