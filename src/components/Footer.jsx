717f79a305c3d1aafc5a2aa6de8c42e1a097dadb
Adarsreg <adarshregmi1@gmail.com>
2023-08-25 01:08:16 -0700

    added fotter

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div id='footer'>
+        Web Building Hackathon, Tech Adhrista 2023, SMIT Sikkim</div>
+  )
+}
+
+export default Footer
