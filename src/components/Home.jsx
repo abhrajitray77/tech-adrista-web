26d03133af42084e588f9b9920fcba72552e2e5d
abhrajitray77 <abhrajitray77@gmail.com>
2023-08-25 00:16:32 -0700

    pics width fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import girl from "../assets/img/elena-joy.png"
-import plant1 from "../assets/img/plant1.png"
-import plant2 from "../assets/img/plant2.png"
-import '../styles/Home.css'
+import React from "react";
+import girl from "../assets/img/elena-joy.png";
+import plant1 from "../assets/img/plant1.png";
+import plant2 from "../assets/img/plant2.png";
+import "../styles/Home.css";
 const Home = () => {
   return (
-    <div>
-        <section class='about'>
-            <h1 class='about-head'>Elena Joy Photography</h1>
-            <p class='about-para'>We have an excellent team to florish your Weddings,
-            Parties, Birthdays, etc. by our excellent quality Photos.
-            Click below to check our Gallery</p>
-            <button class='gallery'>Gallery</button>
-        </section>
-        <div class='girl-img'>
-            <div><img src={girl}/></div>
-            <div><img src={plant1}/></div>
-            <div><img src={plant2}/></div>
-        </div>
+    <div className="home-sec">
+      <section class="about">
+        <h1 class="about-head">Elena Joy Photography</h1>
+        <p class="about-para">
+          We have an excellent team to florish your Weddings, Parties,
+          Birthdays, etc. by our excellent quality Photos. Click below to check
+          our Gallery
+        </p>
+        <button class="gallery">Gallery</button>
+      </section>
+      <section class="girl-img">
+        <img class="girl" src={girl} />
+        <img class="plant1" src={plant1} />
+        <img class="plant2" src={plant2} />
+      </section>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
