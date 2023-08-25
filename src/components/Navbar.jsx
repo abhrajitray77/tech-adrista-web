c9a71f8990db074c1eb2d47601adb697d826e652
abhrajitray77 <abhrajitray77@gmail.com>
2023-08-24 23:04:23 -0700

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Elena Joy</h1>
+        <ul>
+          <li>About</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
